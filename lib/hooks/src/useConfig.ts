39b077478133e03f67fe3c358fbf3992f9c6d7df
dangreco <dan.greco@live.com>
2023-07-28 12:48:44 -0400

    Fix types

--- lib/hooks/src/useConfig.ts
+++ lib/hooks/src/useConfig.ts
@@ -1,10 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useAtom } from 'jotai';
 
 import { Config, atoms } from '@kombu/core';
 
-export function useConfig<
-  T extends Record<string | symbol | number, unknown> = Record<string, never>,
->(): Config<T> | undefined {
+export function useConfig<T extends Record<any, any> = Record<string, never>>():
+  | Config<T>
+  | undefined {
   const [config] = useAtom(atoms.config);
   return config as Config<T> | undefined;
 }
