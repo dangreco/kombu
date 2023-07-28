ff75ffec8566b6089ca0e673dc8aace9c7545c2f
dangreco <dan.greco@live.com>
2023-07-28 12:45:00 -0400

    Fix types

--- lib/hooks/src/useConfig.ts
+++ lib/hooks/src/useConfig.ts
@@ -2,9 +2,9 @@ import { useAtom } from 'jotai';
 
 import { Config, atoms } from '@kombu/core';
 
-export function useConfig<T extends Record<string, unknown> = Record<string, never>>():
-  | Config<T>
-  | undefined {
+export function useConfig<
+  T extends Record<string | symbol | number, unknown> = Record<string, never>,
+>(): Config<T> | undefined {
   const [config] = useAtom(atoms.config);
   return config as Config<T> | undefined;
 }
